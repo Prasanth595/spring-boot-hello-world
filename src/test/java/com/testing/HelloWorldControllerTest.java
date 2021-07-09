be6d31bd7096055e7868e934c162ceb89dc23eb0
Prasanth Arumugam <85020330+Prasanth595@users.noreply.github.com>
2021-07-09 23:58:30 +0530

    Update HelloWorldControllerTest.java

--- src/test/java/com/testing/HelloWorldControllerTest.java
+++ src/test/java/com/testing/HelloWorldControllerTest.java
@@ -1,16 +1,5 @@
 package com.testing;
 
-import static com.testing.HelloWorldController.MESSAGE_KEY;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.net.UnknownHostException;
-import java.util.Map;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
-
 public class HelloWorldControllerTest {
 
 }
