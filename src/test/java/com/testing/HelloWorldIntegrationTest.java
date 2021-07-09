bd25a1e8b9481a97bd43d5cc54d92345bc41e20e
Prasanth Arumugam <85020330+Prasanth595@users.noreply.github.com>
2021-07-09 23:58:48 +0530

    Update HelloWorldIntegrationTest.java

--- src/test/java/com/testing/HelloWorldIntegrationTest.java
+++ src/test/java/com/testing/HelloWorldIntegrationTest.java
@@ -1,22 +1,5 @@
 package com.testing;
 
-import static com.testing.HelloWorldController.MESSAGE_KEY;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import java.util.Map;
-
-import javax.transaction.Transactional;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
-
 public class HelloWorldIntegrationTest {
     
 
